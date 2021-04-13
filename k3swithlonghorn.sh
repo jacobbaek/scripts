23fa3e8b9935492d7e5dd189040bc49e968a373c
jacobbaek <dubaek@gmail.com>
2021-04-13 23:24:56 +0900

    Update k3swithlonghorn.sh

--- k3swithlonghorn.sh
+++ k3swithlonghorn.sh
@@ -11,7 +11,7 @@ DEBUG="no"
 
 set -e
 
-if [[ "$DEBUG" == "yes" ]]; then
+if [[ $DEBUG == "yes" ]]; then
     set -x
 fi
 
@@ -45,9 +45,9 @@ kubectl delete configmap/local-path-config -n kube-system
 
 # longhorn
 
-if [[ "$OSDIST" == *"CentOS"* ]]; then
+if [[ $OSDIST == *"CentOS"* ]]; then
     sudo yum install iscsi-initiator-utils -y
-elif [[ "$OSDIST" == *"Ubuntu"* ]];then
+elif [[ $OSDIST == *"Ubuntu"* ]];then
     sudo apt-get update
     sudo apt install open-iscsi -y
 else
@@ -63,7 +63,7 @@ helm install longhorn longhorn/longhorn -n longhorn-system \
  --set defaultSettings.defaultDataLocality="best-effort"
 
 # velero
-if [[ "$INST_VELERO" == "yes" ]]; then
+if [[ $INST_VELERO == "yes" ]]; then
     curl -L https://github.com/vmware-tanzu/velero/releases/download/v1.5.4/velero-v1.5.4-linux-amd64.tar.gz | tar xvzf -
     cp velero-v1.5.4-linux-amd64/velero /usr/local/bin
 fi
